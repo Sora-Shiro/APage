2fc9c864aeae74ea92fed642d7efbe16c616813c
sorashiro <sora95shiro@gmail.com>
2017-07-15 19:40:15 +0800

    just-test

--- Sora-Shiro-Chart/js/sora-shiro-chart.js
+++ Sora-Shiro-Chart/js/sora-shiro-chart.js
@@ -1,50 +1,93 @@
-
-var mSoraShiroCanvas;
-var mSoraShiroLineCharts = [];
-
-SoraShiroEntry = {
-  isSoraShiroEntry: true,
-  setX: function (X) {
-    this.x = X;
-  },
-  setY: function (Y) {
-    this.y = Y;
-  },
-  getX: function () {
-    return this.x;
-  },
-  getY: function () {
-    return this.y;
-  },
-};
+// SoraShiroEntry = {
+//   isSoraShiroEntry: true,
+//   init: function (X, Y) {
+//     this.setX(X);
+//     this.setY(Y);
+//   },
+//   setX: function (X) {
+//     this.x = X;
+//   },
+//   setY: function (Y) {
+//     this.y = Y;
+//   },
+//   getX: function () {
+//     return this.x;
+//   },
+//   getY: function () {
+//     return this.y;
+//   },
+// };
 
 SoraShiroLineChart = {
   isSoraShiroLineChart: true,
-  setEntrys: function (Entrys) {
-    var i = 0;
-    //TODO judge if Array
-    for(; i < Entrys.length; i++) {
-      if(Entrys[i].isSoraShiroEntry == undefined) {
-        throw new TypeError("Entrys [$i] Not SoraShiroEntry var");
-      }
+  init: function (XLabels, YVals, Color) {
+    this.setXLabels(XLabels);
+    this.setYVals(YVals);
+    this.setColor(Color);
+  },
+  setXLabels: function (XLabels) {
+    if(!isArray(XLabels)) {
+      throw new TypeError("Not Array var");
     }
-    this.entrys = Entrys;
+    this.xLabels = XLabels;
+  }
+  setYVals: function (YVals) {
+    if(!isArray(YVals)) {
+      throw new TypeError("Not Array var");
+    }
+    this.yVals = YVals;
   },
-  getEntrys: function () {
-    return this.entrys;
+  getYVals: function () {
+    return this.yVals;
   },
-  setHeight: function (height) {
-    this.height = height;
+  setColor: function (Color) {
+    this.color = Color;
+  },
+  getColor: function () {
+    return this.color;
   },
 };
 
 SoraShiroCanvas = {
+  isSoraShiroCanvas: true,
+  init: function (Canvas, LineCharts) {
+    this.setCanvas(Canvas);
+    this.setLineCharts(LineCharts);
+  },
   setCanvas: function (Canvas) {
+    if(Canvas.getContext() == undefined) {
+      throw new TypeError("Not HTMLCanvasElement var");
+    }
     this.canvas = Canvas;
   },
   getCanvas: function () {
     return this.canvas;
-  }
+  },
+  setLineCharts: function (LineCharts) {
+    if(LineCharts.isSoraShiroLineChart == undefined) {
+      throw new TypeError("Not SoraShiroLineChart var");
+    }
+    this.lineCharts = LineCharts;
+  },
+  getLineCharts: function () {
+    return this.lineCharts;
+  },
+  drawLineCharts: function () {
+    var context = this.canvas.getContext("2d");
+    var height = this.canvas.height;
+    var width = this.canvas.width;
+    this.lineCharts.map(function (lineChart) {
+      lineChart.xLabels.map(function (xLabel) {
+
+      });
+      yVals.sort();
+      lineChart.yVals.map(function (yVal) {
+
+      });
+    });
+  },
+};
+
+function isArray(obj) { 
+  return Object.prototype.toString.call(obj) === '[object Array]';   
 }
-
-
